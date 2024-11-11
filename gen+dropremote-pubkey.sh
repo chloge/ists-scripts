b6b7abf987c68286f401afd1cc04859db3bc1885
chloge <146779013+chloge@users.noreply.github.com>
2024-11-11 01:43:38 -0500

    Add files via upload

---
+++ gen+dropremote-pubkey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Enter the IP address to forward the public key: " ip_address
+read -p "Enter the username on the remote host: " remote_user
+
+ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
+
+# Ensure the .ssh directory and authorized_keys file exist on the remote host
+ssh $remote_user@$ip_address 'mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys'
+
+
+ssh-copy-id -i ~/.ssh/id_rsa.pub $remote_user@$ip_address
+
+
+echo "Public key forwarded to $remote_user@$ip_address. You can now use the public key for authentication."
+
+
