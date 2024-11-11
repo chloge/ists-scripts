c16e329dedd0faacd61ff0e58ab84676e3c6dad4
chloge <146779013+chloge@users.noreply.github.com>
2024-11-11 01:48:52 -0500

    Update gen+dropremote-pubkey.sh

--- gen+dropremote-pubkey.sh
+++ gen+dropremote-pubkey.sh
@@ -8,9 +8,9 @@ ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
 # Ensure the .ssh directory and authorized_keys file exist on the remote host
 ssh $remote_user@$ip_address 'mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys'
 
-
 ssh-copy-id -i ~/.ssh/id_rsa.pub $remote_user@$ip_address
 
+echo "Private key generated at ~/.ssh/id_rsa"
 
 echo "Public key forwarded to $remote_user@$ip_address. You can now use the public key for authentication."
 
