2cc5097e37d2afaa9023ae6fec72b80c8a7a4aac
chloge <146779013+chloge@users.noreply.github.com>
2024-11-11 01:15:39 -0500

    Add files via upload

---
+++ semaphore-quick-install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Designed for Ubuntu or other apt-based distros.
+# Tested on:
+    # Ubuntu 22.04 LTS Server
+
+# Install Docker
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Run Docker command
+sudo docker run --name semaphore -p 3000:3000 -e SEMAPHORE_DB_DIALECT=bolt -e SEMAPHORE_ADMIN=admin -e SEMAPHORE_ADMIN_PASSWORD=changeme -e SEMAPHORE_ADMIN_NAME="Admin" -e SEMAPHORE_ADMIN_EMAIL=admin@localhost --restart=always -d semaphoreui/semaphore:v2.10.35
+
+# Prompt user to open port 3000
+read -p "Would you like to open port 3000? Only allow this if you need the Semaphore web GUI accessable across the network (y/n): " open_port
+
+if [ "$open_port" = "y" ]; then
+    if command -v iptables > /dev/null 2>&1; then
+        sudo iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
+        sudo iptables-save
+        echo "Port 3000 opened using iptables."
+
+    elif command -v nft > /dev/null 2>&1; then
+        sudo nft add rule inet filter input tcp dport 3000 accept
+        echo "Port 3000 opened using nftables."
+
+    elif command -v firewall-cmd > /dev/null 2>&1; then
+        sudo firewall-cmd --add-port=3000/tcp --permanent
+        sudo firewall-cmd --reload
+        echo "Port 3000 opened using firewalld."
+
+    elif command -v ufw > /dev/null 2>&1; then
+        sudo ufw allow 3000/tcp
+        sudo ufw reload
+        echo "Port 3000 opened using ufw."
+
+    else
+        echo "No compatible firewall configuration tool found."
+    fi
+else
+    echo "Port 3000 remains closed."
+fi
+
+# Display network interfaces and their IP addresses
+ip addr
+
+echo "REMEMBER TO CHANGE SEMAPHORE ADMIN CREDENTIALS!!!!!!!!!"
